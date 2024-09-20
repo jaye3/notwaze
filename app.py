075ff16465399e3763afbb45116cb35d79ba486f
jaye3 <jana.choa@gmail.com>
2024-09-20 13:15:04 +0800

    Removed streamlit-extras dependency

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import requests, json, time
 # Third-party libraries
 import streamlit as st
 from st_keyup import st_keyup
-# from streamlit_extras.switch_page_button import switch_page 
 import folium
 
 # Helper functions (local imports)
