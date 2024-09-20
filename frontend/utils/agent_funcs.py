afe6b6bd37e1e456e1680ebff55d808d164cdf5b
jaye3 <jana.choa@gmail.com>
2024-09-20 23:15:08 +0800

    Added location mapping and adjusted endpoints

--- frontend/utils/agent_funcs.py
+++ frontend/utils/agent_funcs.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import requests
+import requests, json
 
 def agentInit(userData):
     #start and end details are a tuple of (layman address, (lat, long))
     st.session_state.agent_active = True
 
-    url = "http://3.89.63.81:80/collect-user-data" 
+    url = "http://3.89.63.81:80/generate_route" 
     
     try:
         # Send user details to backend API to store info
@@ -13,6 +13,7 @@ def agentInit(userData):
 
         if res.status_code == 200:
             st.success(f"Response from backend: {res.json()['message']}")
+            return res.json()
         else:
             st.error(f"Error: {res.status_code} - {res.text}")
     except Exception as e:
@@ -20,32 +21,19 @@ def agentInit(userData):
     
     return 
 
-def agentRouting():
-    url = "http://3.89.63.81:80/generate_route"
-
-    headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json"
-    }
-
-    try:
-        # Make the POST request
-        response = requests.post(url, headers=headers)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            st.success("Successfully generated route!")
-            return response.json()
-        else:
-            st.error(f"Failed with status code {response.status_code}: {response.text}")
-            return None
+def generateSummary():
+    url = "http://3.89.63.81:80/generate-summary" 
     
-    except requests.exceptions.ConnectionError as conn_err:
-        st.error(f"Connection error: {conn_err}")
-        return None
-    except requests.exceptions.Timeout as timeout_err:
-        st.error(f"Request timed out: {timeout_err}")
-        return None
-    except requests.exceptions.RequestException as req_err:
-        st.error(f"An error occurred: {req_err}")
-        return None
+    try:
+        # Send user details to backend API to store info
+        res = requests.get(url)
+
+        if res.status_code == 200:
+            st.success(f"Response from backend: {res.json()['message']}")
+            return res.json()
+        else:
+            st.error(f"Error: {res.status_code} - {res.text}")
+    except Exception as e:
+        st.error(f"Request failed: {e}")
+    
+    return 
