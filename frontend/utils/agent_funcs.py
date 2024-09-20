f7e8c7013a28a3d001e6fd4d66f00c4323ac0c84
jaye3 <jana.choa@gmail.com>
2024-09-21 01:13:55 +0800

    Changed port and removed timeout

--- frontend/utils/agent_funcs.py
+++ frontend/utils/agent_funcs.py
@@ -4,7 +4,7 @@ import requests, json
 def agentInit(userData):
     st.session_state.agent_active = True
 
-    url = "http://3.210.254.22:8000/generate_route" 
+    url = "http://3.210.254.22:80/generate_route" 
     jsonData = json.dumps(userData)
     
     try:
@@ -12,7 +12,7 @@ def agentInit(userData):
         res = requests.post(url, headers={
             'accept': 'application/json',
             'Content-Type': 'application/json'
-        }, data=jsonData, timeout=8000)
+        }, data=jsonData)
         
         if res.status_code == 200:
             st.success(f"Response from backend: {res.json()['message']}")
